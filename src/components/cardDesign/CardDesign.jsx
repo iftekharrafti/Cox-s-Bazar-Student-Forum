7311153b592612d97e0475f984be506ff1070220
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-09-24 12:26:07 +0600

    [DONE] Alumni,Modal, Footer and home part president countUp

---
+++ src/components/cardDesign/CardDesign.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Col } from "react-bootstrap";
+import Img from "../lazyLoadImage/Img";
+import { baseImgUrl } from "@/utils/imgUrl";
+import Style from "./cardDesign.module.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsFillEyeFill } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import QuickViewModal from "../quickViewModal/QuickViewModal";
+
+const CardDesign = ({ item }) => {
+    const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <Col lg={4} md={6} sm={12}>
+      <div className={Style.cardDesign}>
+        <Img src={baseImgUrl + item?.image} className={Style.cardImg} />
+        <h4 className={Style.name}>{item?.name}</h4>
+        <p className={Style.text}>{item?.text1}</p>
+        <div className={Style.icons}>
+          <div className={Style.icon}>
+            <a href="https://www.facebook.com/">
+              <BsFacebook />
+            </a>
+          </div>
+          <div className={Style.icon} onClick={() => setModalShow(true)}>
+            <BsFillEyeFill />
+          </div>
+          <div className={Style.icon}>
+            <a href="https://www.instagram.com/">
+              <BsInstagram />
+            </a>
+          </div>
+        </div>
+
+        {/* Modal */}
+        <QuickViewModal show={modalShow} onHide={() => setModalShow(false)} item={item} />
+      </div>
+    </Col>
+  );
+};
+
+export default CardDesign;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Footer from "@/components/footer/Footer";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </>
+  );
 }
